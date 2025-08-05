4c38c758fa08e676bdcf03c25c24a2144935fe11
YulikK <yulikpoch@gmail.com>
2025-08-05 11:44:18 +0200

    feat: add buttons

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,29 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CounterService } from './services/counter.service';
+import { CounterButton } from './shared/components/counter-button/counter-button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CounterButton],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[class.counter-container]': 'true',
+  },
 })
 export class App {
-  protected readonly title = signal('modern-angular-app');
+  private readonly counterService = inject(CounterService);
+
+  protected readonly count = this.counterService.count;
+
+  protected onIncrement(): void {
+    this.counterService.increment();
+  }
+
+  protected onDecrement(): void {
+    this.counterService.decrement();
+  }
 }
